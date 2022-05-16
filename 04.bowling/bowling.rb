6b5a6ccbecd276d9efad9b403374dad21a49d503
Kamei <Kameitaiki03@gmail.com>
2022-05-16 16:00:35 +0900

    Add bowling.rb

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,11 +1,11 @@
-require 'debug'
+# frozen_string_literal: true
 
 score = ARGV[0]
-scores = score.split(",")
+scores = score.split(',')
 knocked_pins = []
 
 scores.each do |s|
-  if s == "X"
+  if s == 'X'
     knocked_pins << 10
     knocked_pins << 0
   else
@@ -18,21 +18,19 @@ knocked_pins.each_slice(2) do |s|
   frames << s
 end
 
-if frames[9].sum == 10
-    frames[9] = frames[9..].flatten
-end
+frames[9] = frames[9..].flatten if frames[9].sum == 10
 frames[9].delete(0)
 
 point = 0
 frames[0..8].each_with_index do |frame, i|
-    point +=if frame[0] == 10 && frames[i + 1][0] == 10
-                20 + frames[i + 2][0]
+  point +=  if frame[0] == 10 && frames[i + 1][0] == 10
+              20 + frames[i + 2][0]
             elsif frame[0] == 10
-                10 + frames[i + 1][0] + frames[i + 1][1]
+              10 + frames[i + 1][0] + frames[i + 1][1]
             elsif frame.sum == 10
-                10 + frames[i + 1][0]
+              10 + frames[i + 1][0]
             else
-                frame.sum
+              frame.sum
             end
 end
 

ffbbfac4b14753f3dc92614d1768608d9cacf674
Kamei <Kameitaiki03@gmail.com>
2022-05-18 16:23:18 +0900

    レビューでの指摘された箇所の修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,28 +1,26 @@
 require "date"
 require "optparse"
 
-
 today = Date.today
 options = ARGV.getopts("y:", "m:")
 year  = (options["y"] || today.year).to_i
 month = (options["m"] || today.month).to_i
-first_day = Date.new(year, month, 1)
-last_day = Date.new(year, month, -1)
-first_wday = first_day.wday
-days = (first_day..last_day)
-calender_head = first_day.strftime("%-m月 %Y")
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+first_wday = first_date.wday
+dates = (first_date..last_date)
+calender_head = first_date.strftime("%-m月 %Y")
 day_of_the_week = "日 月 火 水 木 金 土"
 
 puts calender_head.center(20)
 puts day_of_the_week
-print "  " + "   " * (first_wday -1)
+print "   " * first_wday
 
-days.each do |date|
-    if date.sunday?
-        print date.day.to_s.rjust(2)
-    elsif date.saturday?
-        print " " + date.day.to_s.rjust(2) + "\n"
-    else
-        print " " + date.day.to_s.rjust(2)
-    end
+dates.each do |date|
+  formatted_day = date.day.to_s.rjust(2)
+  if date.saturday?
+    print formatted_day + "\n"
+  else
+    print formatted_day + " "
+  end
 end

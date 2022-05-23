bc732a69a07bdb79aafaeee1a99f532fa1e01e57
Kamei <Kameitaiki03@gmail.com>
2022-05-23 17:12:09 +0900

    レビューで指摘された箇所の修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -13,27 +13,27 @@ scores.each do |s|
   end
 end
 
-frames = []
-knocked_pins.each_slice(2) do |s|
+frames = knocked_pins.each_slice(2).to_a do |s|
   frames << s
 end
 
-frames[9] = frames[9..].flatten if frames[9].sum == 10
-frames[9].delete(0)
+LAST_FRAME_INDEX = 9
 
-point = 0
-frames[0..8].each_with_index do |frame, i|
-  point +=  if frame[0] == 10 && frames[i + 1][0] == 10
-              20 + frames[i + 2][0]
-            elsif frame[0] == 10
-              10 + frames[i + 1][0] + frames[i + 1][1]
-            elsif frame.sum == 10
-              10 + frames[i + 1][0]
-            else
-              frame.sum
-            end
+frames[LAST_FRAME_INDEX] = frames[LAST_FRAME_INDEX..].flatten if frames[LAST_FRAME_INDEX].sum == 10
+frames[LAST_FRAME_INDEX].delete(0)
+
+point = frames[0..8].each_with_index.sum do |frame, i|
+  if frame[0] == 10 && frames[i + 1][0] == 10
+    20 + frames[i + 2][0]
+  elsif frame[0] == 10
+    10 + frames[i + 1][0] + frames[i + 1][1]
+  elsif frame.sum == 10
+    10 + frames[i + 1][0]
+  else
+    frame.sum
+  end
 end
 
-point += frames[9].sum
+point += frames[LAST_FRAME_INDEX].sum
 
 puts point

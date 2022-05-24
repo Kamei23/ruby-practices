6003fddca0e3b38d82bc4422501a74afdcd4bcad
Kamei <Kameitaiki03@gmail.com>
2022-05-24 18:17:39 +0900

    レビューで指摘された箇所を再度修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -22,7 +22,7 @@ LAST_FRAME_INDEX = 9
 frames[LAST_FRAME_INDEX] = frames[LAST_FRAME_INDEX..].flatten if frames[LAST_FRAME_INDEX].sum == 10
 frames[LAST_FRAME_INDEX].delete(0)
 
-point = frames[0..8].each_with_index.sum do |frame, i|
+point = frames[0..LAST_FRAME_INDEX - 1].each_with_index.sum do |frame, i|
   if frame[0] == 10 && frames[i + 1][0] == 10
     20 + frames[i + 2][0]
   elsif frame[0] == 10
